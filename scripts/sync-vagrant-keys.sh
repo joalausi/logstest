34b9d296d3014c0f588f87962e142870ffa8f448
The Most Positive One <artchornonog@gmail.com>
2026-07-20 01:58:09 +0300

    feat: sync monitoring VM SSH key

--- scripts/sync-vagrant-keys.sh
+++ scripts/sync-vagrant-keys.sh
@@ -5,7 +5,7 @@ KEY_DIR="$HOME/.ssh/automation-alchemy"
 
 mkdir -p "$KEY_DIR"
 
-for host in lb-01 web-01 web-02 app-01 ci-01; do
+for host in lb-01 web-01 web-02 app-01 ci-01 monitoring-01; do
   src=".vagrant/machines/$host/virtualbox/private_key"
   dst="$KEY_DIR/$host"
 
